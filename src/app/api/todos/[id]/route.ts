89d0149bc258199695f401ee3ee6c483f3365539
imuhammadusmanali <iusmanali8@gmail.com>
2023-05-08 06:47:16 -0400

    features commit

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -3,7 +3,8 @@ import { NextResponse } from 'next/server';
 const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos';
 
 export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+  // const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+  const id = '1';
 
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
 
