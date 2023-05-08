95c7dcc9b4a3404d58d089b0c91198ef61554385
imuhammadusmanali <iusmanali8@gmail.com>
2023-05-08 16:51:54 -0400

    added middleware

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,8 @@
+import { limiter } from '../config/limiter';
+
 export async function GET() {
+  const remaining = await limiter.removeTokens(1);
+  console.log('remaining', remaining);
+
   return new Response('Hello NextJs!');
 }
